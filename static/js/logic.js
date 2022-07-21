50a05f34b1c759cdebb3be2b4ecd4386870a43f7
William Abate <williamabate6@gmail.com>
2022-07-20 21:24:08 -0500

    Working Commit 4
    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,3 +4,20 @@ d3.json(queryUrl).then(function (data) {
     console.log(data)
     createFeatures(data.features);
 });
+
+function createFeatures(earthquakeData) {
+
+    function onEachFeature(features, layer) {
+      layer.bindPopup(`<h3>${features.properties.place}</h3><hr><p>${new Date(features.properties.time)}</p>`);
+    }
+  // Creating colors for earthquake size chart according to the earthquake size and magnitude(0-2.5, 2.5-5, 5-7, 7+)
+    function changeColor(features) {
+      if (features.properties.mag > 7)
+      return 'red'
+      else if (features.properties.mag > 5)
+      return 'yellow'
+      else if (features.properties.mag > 2.5)
+      return 'yellowgreen'
+      else 
+      return 'green'
+    };
